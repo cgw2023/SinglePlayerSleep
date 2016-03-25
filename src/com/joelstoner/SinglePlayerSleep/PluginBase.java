c81129db467029a4285b1b4bc60caddd192f1191
JoelGodOfwar <joelstoner@gmail.com>
2016-03-25 03:07:03 -0500

    v2.13.2

--- src/com/joelstoner/SinglePlayerSleep/PluginBase.java
+++ src/com/joelstoner/SinglePlayerSleep/PluginBase.java
@@ -1,8 +1,13 @@
 package com.joelstoner.SinglePlayerSleep;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -15,11 +20,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.mcstats.MetricsLite;
 import org.spigotmc.Metrics;
+
+import com.joelstoner.SinglePlayerSleep.api.Updater;
+import com.joelstoner.SinglePlayerSleep.api.Updater.UpdateResult;
+import com.joelstoner.SinglePlayerSleep.api.Updater.UpdateType;
 
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
@@ -30,6 +40,8 @@ import net.md_5.bungee.api.chat.TextComponent;
 @SuppressWarnings("unused")
 public class PluginBase extends JavaPlugin implements Listener{
 
+	public boolean UpdateCheck = true;
+	private boolean UpdateAviable = false;
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public boolean isCanceled = false;
 	public int transitionTask = 0;
@@ -42,6 +54,9 @@ public class PluginBase extends JavaPlugin implements Listener{
 		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " Has been enabled");
 		this.logger.info("**************************************************************");
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		Reloadconfig();
+		
 		try {
 			MetricsLite metrics = new MetricsLite(this);
 			metrics.start();
@@ -124,22 +139,54 @@ public class PluginBase extends JavaPlugin implements Listener{
 		if(world.hasStorm()){
 			if(player.hasPermission("sps.downfall")){
 				world.setStorm(false);
+				player.sendMessage("Clearing downfall...");
 				this.log("clearing downfall..."); //Logger
 			}
 		}
 		if(world.isThundering()){
 			if(player.hasPermission("sps.thunder")){
 				world.setThundering(false);
+				player.sendMessage("Clearing thunderstorm...");
 				this.log("clearing thunderstorm..."); //Logger
 			}
 		}
 		long Relative_Time = 24000 - world.getTime();
 		world.setFullTime(world.getFullTime() + Relative_Time);
+		player.sendMessage("Setting time to day...");
 		this.log("Setting time to day..."); //Logger
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		Player p = (Player)sender;
+	    if (cmd.getName().equalsIgnoreCase("SPS"))
+	    {
+	      if (args.length == 0)
+	      {
+	        p.sendMessage("§a[]===============[§4§lSinglePlayerSleep§a]===============[]");
+	        p.sendMessage("§4 Sleep in a bed to use.");
+	        p.sendMessage("§4 ");
+	        p.sendMessage("§4 /Sleep - subject to server admin approval");
+	        p.sendMessage("§4 /Cancel - Cancels SinglePlayerSleep");
+	        p.sendMessage("§a[]===============[§4§lSinglePlayerSleep§a]===============[]");
+	        return true;
+	      }
+		  if(args[0].equalsIgnoreCase("reload")){
+			  Reloadconfig();
+	      }
+		  if(args[0].equalsIgnoreCase("update")){
+			  // Player must be OP and auto-update-check must be true
+			if(p.isOp() && UpdateCheck){	
+			    Updater updater = new Updater(this, 98534, this.getFile(), UpdateType.NO_DOWNLOAD, true);
+				if (updater.getResult() == UpdateResult.UPDATE_AVAILABLE) {
+					p.sendMessage("New version available! " + updater.getLatestName());
+				}
+			}else{
+				p.sendMessage(p.getDisplayName() + " You are not OP, or auto-update-check is set to false in config.yml");
+			}
+		  }
+	    }
+		
 		if(cmd.getName().equalsIgnoreCase("cancel")){
 			Player player = (Player) sender;
 			if(player.hasPermission("sps.cancel")) {
@@ -151,6 +198,45 @@ public class PluginBase extends JavaPlugin implements Listener{
 			}
 			if(isCanceled){
 				isCanceled = false;
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("sleep")){
+			Player player = (Player) sender;
+			if(player.hasPermission("sps.command")) {
+				final Player player1 = (Player) sender;
+				final World world = player1.getWorld();
+				//Broadcast to Server
+				TextComponent message2 = new TextComponent(player1.getDisplayName() + " is sleeping <command>");
+				TextComponent message = new TextComponent("[CANCEL] ");
+				message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/cancel"));
+				message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to cancel sleep").create()));
+				//this.broadcast(player.getDisplayName() + " is sleeping ");
+				message.addExtra(message2);
+				this.broadcast(message);
+				//this.broadcast(message);
+				//player.sendMessage( message );
+				if(player1.hasPermission("sps.hermits")){
+					//Thread.sleep(10000);
+					if(!isCanceled){
+						
+						transitionTask = this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+							public void run() {
+								// TODO Auto-generated method stub
+								//getLogger().info("runnable");
+								setDatime(player1, world);
+							}
+							
+						}, 10 * 20);
+						
+					}else{
+						
+						isCanceled = false;
+					}
+					//player.sendMessage(ChatColor.RED + "isCanceled=" + isCanceled);
+				}else if(!player1.hasPermission("sps.hermits")){
+					player1.sendMessage("You do not have permission to Single Player Sleep!");
+				}
 			}
 		}
 
@@ -167,4 +253,24 @@ public class PluginBase extends JavaPlugin implements Listener{
 	public void broadcast(TextComponent message){
 		Bukkit.getServer().spigot().broadcast(message);
 	}
+	  
+	@EventHandler
+	public void onPlayerJoinEvent(PlayerJoinEvent e)
+	  {
+	    Player p = e.getPlayer();
+	    if ((p.isOp())  && 
+	      (UpdateAviable) )//&& 
+	      //(this.plugin.getConfig().getBoolean("SinglePlayerSleep.AutoUpdater")))
+	    {
+	      p.sendMessage("§4[§aSinglePlayerSleep§4] §a-=> Update is available! <=-");
+	      p.sendMessage("§aDownload: §ehttp://www.spigotmc.org/resources/SinglePlayerSleep-1-8-with-gui.7416/");
+	    }
+	}
+	public void Reloadconfig(){
+		// Load config.
+		FileConfiguration config = getConfig();
+		reloadConfig();
+		UpdateCheck = config.getBoolean("auto-update-check");
+	}
+
 }
