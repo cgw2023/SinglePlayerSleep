b41fc64da64cb107bd8093547437c8da03e22fb4
JoelGodOfwar <joelstoner@gmail.com>
2016-03-31 01:42:56 -0500

    v2.13.7

--- src/com/joelstoner/SinglePlayerSleep/PluginBase.java
+++ src/com/joelstoner/SinglePlayerSleep/PluginBase.java
@@ -1,6 +1,11 @@
 package com.joelstoner.SinglePlayerSleep;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -11,12 +16,15 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.logging.Logger;
 
+import org.apache.commons.codec.language.bm.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -33,32 +41,77 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 
+import com.joelstoner.SinglePlayerSleep.api.ChatColorUtils;
+import com.joelstoner.SinglePlayerSleep.api.ConfigAPI;
+
+
 
 @SuppressWarnings("unused")
 public class PluginBase extends JavaPlugin implements Listener{
 
-	public boolean UpdateCheck;
-	public boolean debug;
+	public static boolean UpdateCheck;
+	public static boolean cancelbroadcast;
+	public static boolean debug;
+	public static String daLang;
 	private boolean UpdateAviable = false;
-	public final Logger logger = Logger.getLogger("Minecraft");
+	public final static Logger logger = Logger.getLogger("Minecraft");
 	public boolean isCanceled = false;
 	public int transitionTask = 0;
 	private URL url;
-	private static long mobSpawningStartTime = 13187;
+	private static long mobSpawningStartTime = 13000;
 	//mobs stop spawning at: 22813
 	//mobs start to burn at: 23600
 	private static long mobSpawningStopTime = 23600;
+	File langFile;
+    FileConfiguration lang;
+    
 	
 	@Override // TODO:
 	public void onEnable(){
-		CheckForConfig();
+		langFile = new File(getDataFolder(), "lang.yml");
+		if(!langFile.exists()){                                  // checks if the yaml does not exists
+			langFile.getParentFile().mkdirs();                  // creates the /plugins/<pluginName>/ directory if not found
+			ConfigAPI.copy(getResource("lang.yml"), langFile); // copies the yaml from your jar to the folder /plugin/<pluginName>
+        }
+		lang = new YamlConfiguration();
+		try {
+			lang.load(langFile);
+		} catch (IOException | InvalidConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ConfigAPI.CheckForConfig(this);
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info("**************************************************************");
-		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " Has been enabled");
-		this.logger.info("**************************************************************");
+		PluginBase.logger.info("**************************************************************");
+		PluginBase.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " Has been enabled");
+		PluginBase.logger.info("**************************************************************");
 		getServer().getPluginManager().registerEvents(this, this);
 		
-		Reloadconfig();
+		String varCheck = getConfig().getString("auto-update-check");
+		String varCheck2 = getConfig().getString("cancelcolor");
+		String varCheck3 = getConfig().getString("cancelbroadcast");
+		String varCheck4 = getConfig().getString("debug");
+		String varCheck5 = getConfig().getString("lang");
+		//log("varCheck " + varCheck);
+		//log("varCheck2 " + varCheck2);
+		//log("varCheck3 " + varCheck3);
+		if(varCheck.contains("default")){
+			getConfig().set("auto-update-check", true);
+		}
+		if(varCheck2.contains("default")){
+			getConfig().set("cancelcolor", "RED");
+		}
+		if(varCheck3.contains("default")){
+			getConfig().set("cancelbroadcast", true);
+		}
+		if(varCheck4.contains("default")){
+			getConfig().set("debug", false);
+		}
+		if(varCheck5.contains("default")){
+			getConfig().set("lang", "en_US");
+		}
+		saveConfig();
+		ConfigAPI.Reloadconfig(this);
 		
 		try {
 			MetricsLite metrics = new MetricsLite(this);
@@ -72,51 +125,30 @@ public class PluginBase extends JavaPlugin implements Listener{
 	@Override // TODO:
 	public void onDisable(){
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info("**************************************************************");
-		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " Has been disabled");
-		this.logger.info("**************************************************************");
+		PluginBase.logger.info("**************************************************************");
+		PluginBase.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " Has been disabled");
+		PluginBase.logger.info("**************************************************************");
 		
 	}
 	
-	private void CheckForConfig() {
-		try{
-			PluginDescriptionFile pdfFile = this.getDescription();
-			if(!getDataFolder().exists()){
-				this.log(pdfFile.getName() + ": Data Folder doesn't exist"); //Logger
-				this.log(pdfFile.getName() + ": Creating Data Folder"); //Logger
-				getDataFolder().mkdirs();
-				this.log(pdfFile.getName() + ": Data Folder Created at " + getDataFolder()); //Logger
-			}
-			File  file = new File(getDataFolder(), "config.yml");
-			getLogger().info("" + file);
-			if(!file.exists()){
-				this.log(pdfFile.getName() + ": config.yml not found, creating!"); //Logger
-				saveDefaultConfig();
-				FileConfiguration config = getConfig();
-				
-				config.options().copyDefaults(true);
-				saveConfig();
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+	
+	
 	
 	@EventHandler
 	public void PlayerIsSleeping(PlayerBedEnterEvent event) throws InterruptedException{
 		final Player player = event.getPlayer();
 		final World world = player.getWorld();
 		//Broadcast to Server
-		TextComponent message2 = new TextComponent(player.getDisplayName() + " is sleeping");
-		TextComponent message = new TextComponent("[CANCEL] ");
+		TextComponent message2 = new TextComponent(player.getDisplayName() + " " + lang.get("issleep." + daLang + ""));
+		TextComponent message = new TextComponent(ChatColorUtils.setColors(getConfig().getString("cancelcolor")) + " [" + lang.get("cancel." + daLang + "") + "]");
 		message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/cancel"));
-		message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to cancel sleep").create()));
+		message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("" + lang.get("clickcancel." + daLang + "")).create()));
 		//this.broadcast(player.getDisplayName() + " is sleeping ");
-		message.addExtra(message2);
-		this.broadcast(message);
+		message2.addExtra(message);
+		this.broadcast(message2);
 		//this.broadcast(message);
 		//player.sendMessage( message );
-		if(player.hasPermission("sps.hermits")){
+		if(player.hasPermission("sps.hermits")||player.hasPermission("sps.op")){
 			//Thread.sleep(10000);
 			if(!isCanceled){
 				
@@ -134,29 +166,34 @@ public class PluginBase extends JavaPlugin implements Listener{
 				isCanceled = false;
 			}
 			//player.sendMessage(ChatColor.RED + "isCanceled=" + isCanceled);
-		}else if(!player.hasPermission("sps.hermits")){
-			player.sendMessage("You do not have permission to Single Player Sleep!");
+		}else if(!player.hasPermission("sps.hermits")||player.hasPermission("sps.op")){
+			player.sendMessage("" + lang.get("noperm." + daLang + ""));
 		}
 	}
+
 	public void setDatime(Player player, World world){
 		if(world.hasStorm()){
-			if(player.hasPermission("sps.downfall")){
+			if(player.hasPermission("sps.downfall")||player.hasPermission("sps.op")){
 				world.setStorm(false);
-				player.sendMessage("Clearing downfall...");
-				this.log("clearing downfall..."); //Logger
+				if(debug){
+					log("" + lang.get("setdownfall." + daLang + "") + "...");
+					} //TODO: Logger
 			}
 		}
 		if(world.isThundering()){
-			if(player.hasPermission("sps.thunder")){
+			if(player.hasPermission("sps.thunder")||player.hasPermission("sps.op")){
 				world.setThundering(false);
-				player.sendMessage("Clearing thunderstorm...");
-				this.log("clearing thunderstorm..."); //Logger
+				if(debug){
+					log("" + lang.get("setthunder." + daLang + "") + "...");
+					} //TODO: Logger
 			}
 		}
 		long Relative_Time = 24000 - world.getTime();
 		world.setFullTime(world.getFullTime() + Relative_Time);
-		player.sendMessage("Setting time to day...");
-		this.log("Setting time to day..."); //Logger
+		log("" + debug);
+		if(debug){
+			log("" + lang.get("settime." + daLang + "") + "...");
+			} //TODO: Logger
 	}
 	
 	@Override
@@ -167,16 +204,16 @@ public class PluginBase extends JavaPlugin implements Listener{
 	      if (args.length == 0)
 	      {
 	        p.sendMessage(ChatColor.GREEN + "[]===============[" + ChatColor.YELLOW + "SinglePlayerSleep" + ChatColor.GREEN + "]===============[]");
-	        p.sendMessage(ChatColor.GREEN + " Sleep in a bed to use.");
+	        p.sendMessage(ChatColor.GREEN + " " + lang.get("touse." + daLang + ""));//Sleep in a bed to use.");
 	        p.sendMessage(ChatColor.WHITE + " ");
-	        p.sendMessage(ChatColor.WHITE + " /Sleep - subject to server admin approval");
-	        p.sendMessage(ChatColor.WHITE + " /Cancel - Cancels SinglePlayerSleep");
+	        p.sendMessage(ChatColor.WHITE + " /Sleep - " + lang.get("sleephelp." + daLang + ""));//subject to server admin approval");
+	        p.sendMessage(ChatColor.WHITE + " /Cancel - " + lang.get("cancelhelp." + daLang + ""));//Cancels SinglePlayerSleep");
 	        p.sendMessage(ChatColor.WHITE + " ");
-	        if(p.isOp()){
+	        if(p.isOp()||p.hasPermission("sps.op")){
 		        p.sendMessage(ChatColor.GOLD + " OP Commands");
-		        p.sendMessage(ChatColor.GOLD + " /SPS update - Check for update.");
-		        p.sendMessage(ChatColor.GOLD + " /SPS reload -  Reload config file.");
-		        p.sendMessage(ChatColor.GOLD + " /SPS check true/false - set auto-update-check to true or false.");
+		        p.sendMessage(ChatColor.GOLD + " /SPS update - " + lang.get("spsupdate." + daLang + ""));//Check for update.");
+		        p.sendMessage(ChatColor.GOLD + " /SPS reload - " + lang.get("spsreload." + daLang + ""));//Reload config file.");
+		        p.sendMessage(ChatColor.GOLD + " /SPS check true/false - " + lang.get("spscheck." + daLang + ""));//set auto-update-check to true or false.");
 	        }
 	        p.sendMessage(ChatColor.GREEN + "[]===============[" + ChatColor.YELLOW + "SinglePlayerSleep" + ChatColor.GREEN + "]===============[]");
 	        return true;
@@ -185,31 +222,35 @@ public class PluginBase extends JavaPlugin implements Listener{
 	    	  if(args.length< 1){
 					return false;
 			  }
-	    	  if(!args[1].equalsIgnoreCase("true") & !args[1].equalsIgnoreCase("false")){
-					sender.sendMessage(ChatColor.YELLOW + this.getName() + " Â§cArgument must be boolean. Usage: /sps check True/False");
-				}else if(args[1].contains("true") || args[1].contains("false")){
-					FileConfiguration config = getConfig();
-					config.set("auto-update-check", "" + args[1]);
-					
-					saveConfig();
-					Reloadconfig();
-					sender.sendMessage(ChatColor.YELLOW + this.getName() + " auto-update-check has been set to " + args[1]);
-					if(args[1].contains("false")){
-						sender.sendMessage(ChatColor.YELLOW + this.getName() + " will not check for updates!");
-					}else if(args[1].contains("true")){
-						sender.sendMessage(ChatColor.YELLOW + this.getName() + " will check for updates!");
+	    	  if(p.isOp()||p.hasPermission("sps.op")){
+	    		  if(!args[1].equalsIgnoreCase("true") & !args[1].equalsIgnoreCase("false")){
+						sender.sendMessage(ChatColor.YELLOW + this.getName() + " §c" + lang.get("boolean." + daLang + "") + ": /sps check True/False");
+	    		  }else if(args[1].contains("true") || args[1].contains("false")){
+	    			    FileConfiguration config = getConfig();
+						config.set("auto-update-check", "" + args[1]);
+						
+						saveConfig();
+						ConfigAPI.Reloadconfig(this);
+						sender.sendMessage(ChatColor.YELLOW + this.getName() + " " + lang.get("checkset." + daLang + "") + " " + args[1]);
+						if(args[1].contains("false")){
+							sender.sendMessage(ChatColor.YELLOW + this.getName() + " " + lang.get("nocheck." + daLang + ""));
+						}else if(args[1].contains("true")){
+							sender.sendMessage(ChatColor.YELLOW + this.getName() + " " + lang.get("yescheck." + daLang + ""));
+						}
+						reloadConfig();
+						return true;
 					}
-					reloadConfig();
-					return true;
-				}
+	    	  }
 	    	  
 	      }
 		  if(args[0].equalsIgnoreCase("reload")){
-			  Reloadconfig();
+			  if(p.isOp()||p.hasPermission("sps.op")){
+				  ConfigAPI.Reloadconfig(this);
+			  }
 	      }
 		  if(args[0].equalsIgnoreCase("update")){
 			  // Player must be OP and auto-update-check must be true
-			if(p.isOp() && UpdateCheck){	
+			if(p.isOp() && UpdateCheck||p.hasPermission("sps.op") && UpdateCheck){	
 				try {
 				
 					URL url = new URL("https://raw.githubusercontent.com/JoelGodOfwar/SinglePlayerSleep/master/version.txt");
@@ -221,9 +262,9 @@ public class PluginBase extends JavaPlugin implements Listener{
 		            if(debug){this.log("response= ." + response + ".");} //TODO: Logger
 		            if(debug){this.log("localVersion= ." + localVersion + ".");} //TODO: Logger
 		            if (!response.equalsIgnoreCase(localVersion)) {
-						p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.RED + " New version available!");
+						p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.RED + " " + lang.get("newvers." + daLang + ""));
 					}else{
-						p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.GREEN + " Version is up to date!");
+						p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.GREEN + " " + lang.get("curvers." + daLang + ""));
 					}
 				} catch (MalformedURLException e) {
 					this.log("MalformedURLException");
@@ -237,17 +278,17 @@ public class PluginBase extends JavaPlugin implements Listener{
 				}
 				
 			}else{
-				p.sendMessage(ChatColor.YELLOW + this.getName() + " " + p.getDisplayName() + " You are not OP, or auto-update-check is set to false in config.yml");
+				p.sendMessage(ChatColor.YELLOW + this.getName() + " " + p.getDisplayName() + " " + lang.get("notop." + daLang + ""));
 			}
 		  }
 	    }
 		
 		if(cmd.getName().equalsIgnoreCase("cancel")){
 			Player player = (Player) sender;
-			if(player.hasPermission("sps.cancel")) {
+			if(player.hasPermission("sps.cancel") && IsNight(player.getWorld())||player.hasPermission("sps.op") && IsNight(player.getWorld())) {
 				Bukkit.getScheduler().cancelTask(transitionTask);
-				//Broadcast to Server
-				this.broadcast(player.getDisplayName() +  " canceled sleeping");
+				//Broadcast to Serve.r
+				if(cancelbroadcast){this.broadcast(player.getDisplayName() +  " " + lang.get("canceledsleep." + daLang + ""));}
 				isCanceled = true;
 				
 			}
@@ -259,21 +300,21 @@ public class PluginBase extends JavaPlugin implements Listener{
 			Player player = (Player) sender;
 			World w = player.getWorld();
 			if(!IsNight(w)){
-				player.sendMessage("It must be night to sleep. " + IsNight(w));
+				player.sendMessage("" + lang.get("mustbenight." + daLang + ""));
 				return false;
 			}
 			
-			if(player.hasPermission("sps.command")) {
+			if(player.hasPermission("sps.command")||player.hasPermission("sps.op")) {
 				final Player player1 = (Player) sender;
 				final World world = player1.getWorld();
 				//Broadcast to Server
-				TextComponent message2 = new TextComponent(player1.getDisplayName() + " is sleeping <command>");
-				TextComponent message = new TextComponent("[CANCEL] ");
+				TextComponent message2 = new TextComponent(player1.getDisplayName() + " " + lang.get("sleepcommand." + daLang + ""));
+				TextComponent message = new TextComponent(ChatColorUtils.setColors(getConfig().getString("cancelcolor")) + " [" + lang.get("cancel." + daLang + "") + "]");
 				message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/cancel"));
-				message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to cancel sleep").create()));
+				message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("" + lang.get("clickcancel." + daLang + "")).create()));
 				//this.broadcast(player.getDisplayName() + " is sleeping ");
-				message.addExtra(message2);
-				this.broadcast(message);
+				message2.addExtra(message);
+				this.broadcast(message2);
 				//this.broadcast(message);
 				//player.sendMessage( message );
 				if(player1.hasPermission("sps.hermits")){
@@ -296,16 +337,18 @@ public class PluginBase extends JavaPlugin implements Listener{
 					}
 					//player.sendMessage(ChatColor.RED + "isCanceled=" + isCanceled);
 				}else if(!player1.hasPermission("sps.hermits")){
-					player1.sendMessage("You do not have permission to Single Player Sleep!");
+					player1.sendMessage("" + lang.get("noperm." + daLang + ""));
 				}
+			}else{
+				sender.sendMessage("" + lang.get("noperm." + daLang + ""));
 			}
 		}
 
 		return true;
 	}
 	
-	public void log(String dalog){
-		this.logger.info(this.getName() + " " + dalog);
+	public  void log(String dalog){
+		PluginBase.logger.info(this.getName() + " " + dalog);
 	}
 	
 	public void broadcast(String message){
@@ -331,7 +374,7 @@ public class PluginBase extends JavaPlugin implements Listener{
 	            if(debug){this.log("response= ." + response + ".");} //TODO: Logger
 	            if(debug){this.log("localVersion= ." + localVersion + ".");} //TODO: Logger
 	            if (!response.equalsIgnoreCase(localVersion)) {
-					p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.RED + " New version available!");
+					p.sendMessage(ChatColor.YELLOW + this.getName() + ChatColor.RED + " " + lang.get("newvers." + daLang + ""));
 				}
 			} catch (MalformedURLException e) {
 				this.log("MalformedURLException");
@@ -346,25 +389,7 @@ public class PluginBase extends JavaPlugin implements Listener{
 			
 		}
 	}
-	public void Reloadconfig(){
-		// Load config.
-		FileConfiguration config = getConfig();
-		String daString = config.getString("debug").replace("'", "") + ",";
-		
-		if(daString.contains("true")){
-			debug = true;
-		}else{
-			debug = false;
-		}
-		String daString2 = config.getString("auto-update-check").replace("'", "") + ",";
-		if(daString2.contains("true")){
-			UpdateCheck = true;
-		}else{
-			UpdateCheck = false;
-		}
-		
-		if(debug){this.log("UpdateCheck = " + UpdateCheck);} //TODO: Logger
-	}
+
 	
 	public static boolean IsNight(World w)
     {
